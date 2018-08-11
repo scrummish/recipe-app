f304049f5465fda2d7c259d01017334b56dc1b98
sergio <scrummish@gmail.com>
2018-08-11 01:51:17 -0500

    Added recipe model and created an array of recipes

--- src/app/recipe-book/recipe-list/recipe-list.component.ts
+++ src/app/recipe-book/recipe-list/recipe-list.component.ts
@@ -1,11 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Recipe } from '../recipe.model';
+
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.scss']
 })
 export class RecipeListComponent implements OnInit {
+  // Recipe() Class takes a name, description, and image url all type string 
+  recipes: Recipe[] = [
+    new Recipe('Green Spaghetti', 'Creamy spaghetti', 'https://hips.hearstapps.com/del.h-cdn.co/assets/18/04/1517007351-delish-kids-rainbow-spaghetti-001.jpg' ),
+    new Recipe('Green Spaghetti', 'Creamy spaghetti', 'https://hips.hearstapps.com/del.h-cdn.co/assets/18/04/1517007351-delish-kids-rainbow-spaghetti-001.jpg' ),
+    new Recipe('Green Spaghetti', 'Creamy spaghetti', 'https://hips.hearstapps.com/del.h-cdn.co/assets/18/04/1517007351-delish-kids-rainbow-spaghetti-001.jpg' )
+  ];
 
   constructor() { }
 
---
+++ src/app/recipe-book/recipe.model.ts
@@ -0,0 +1,12 @@
+export class Recipe {
+    public name: string;
+    public description: string;
+    public imagePath: string;
+    // public ingredients: 
+
+    constructor(name: string, description: string, imagePath: string) {
+        this.name = name;
+        this.description = description;
+        this.imagePath = imagePath;
+    }
+}
